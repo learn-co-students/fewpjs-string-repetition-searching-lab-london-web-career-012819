006f9f6a0bb04dd69c72f165410721ff82c20575
Kian Edalat <akedalat@gmail.com>
2019-03-23 17:44:10 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution here
+function heyJude(lyric){
+    return lyric.repeat(16)
+}
+
+function prohibitedLanguage(safeName){
+    let restricted = /candycorn|brusselssprouts/
+    return safeName.search(restricted)
+}
